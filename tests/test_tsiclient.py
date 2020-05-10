cef809584b2cd1e87641cdf3de379a72f9775c3b
sujitkoley9 <sujitkoley9@gmail.com>
2020-05-10 10:36:39 +0530

    hi

--- tests/test_tsiclient.py
+++ tests/test_tsiclient.py
@@ -1,4 +1,11 @@
 import pytest
+import requests
+import pandas as pd
+from collections import namedtuple
+from test_sample.exceptions import TSIEnvironmentError, TSIStoreError, TSIQueryError
+
+
+import pytest
 import math 
 def test_sqrt():   
     num = 25   
