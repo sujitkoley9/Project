7acd260a7db81f877025a2b42d8c90964469abe6
sujitkoley9 <sujitkoley9@gmail.com>
2020-05-10 11:50:21 +0530

    hi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 setup(
   name = 'test_sample',
   packages = ['test_sample'],
-  version = '1.0.4',
+  version = '1.0.5',
   license='MIT',
   long_description=long_description,
   long_description_content_type='text/markdown',
